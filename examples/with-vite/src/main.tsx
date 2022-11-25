ce8a15f049243f4ff9db8cc989a3626bd07511b4
pepe_fabra_valverde <jofaval@outlook.com>
2022-11-25 19:06:11 +0100

    feat: upload bundling experimentations

--- examples/with-vite/src/main.tsx
+++ examples/with-vite/src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+Provider;
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
