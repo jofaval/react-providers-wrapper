1123a429808c5367d326c7e9dd796e4456e798e5
pepe_fabra_valverde <jofaval@outlook.com>
2022-12-03 16:11:36 +0100

    fix: Remove errors from build

--- package/react-providers-wrapper/vite.config.ts
+++ package/react-providers-wrapper/vite.config.ts
@@ -1,26 +1,20 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
-import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [dts()],
   build: {
+    sourcemap: "inline",
     lib: {
-      // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, "src/index.ts"),
       name: "React providers wrapper",
-      // the proper extensions will be added
       fileName: "index",
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ["react"],
+      external: ["react", "react-dom"],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
         globals: {
           react: "React",
         },
