d89e125721cd809f702fe795adcdbd616d395753
pepe_fabra_valverde <jofaval@outlook.com>
2022-12-03 20:35:14 +0100

    fix(docs): Use appropiate `baseUrl`

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: "My Site",
   tagline: "Dinosaurs are cool",
   url: "https://jofaval.github.io/",
-  baseUrl: "/",
+  baseUrl: "/react-providers-wrapper/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
